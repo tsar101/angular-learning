d4d70323523d0390d1d613149ad87c7327d8c3a6
Animesh Chhabra <animesh.chhabra@snapdeal.com>
2017-02-15 10:10:44 +0530

    Learning Angular

--- app/app.js
+++ app/app.js
@@ -3,9 +3,14 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
+  'ngAnimate',
+  'ngSanitize',
+  'ui.bootstrap.demo1',
+  'CarousalDemo',
+  'ui.bootstrap.demo',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'ui.bootstrap'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,38 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('ui.bootstrap.demo1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'AccordionDemoCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('AccordionDemoCtrl', ['$scope',function ($scope) {
+  $scope.oneAtATime = true;
 
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3','Item 67'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isCustomHeaderOpen: false,
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
 }]);
